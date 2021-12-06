7348699d7568c5ac9e6785497990f761d95da8d5
lisahell <lisa.hell03@htl-salzburg.ac.at>
2021-12-06 23:25:32 +0100

    add Texts and Score including script to count it

---
+++ Recap-lhell/Assets/MyGame/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scoreText;
+    private int score;
+
+    public GameObject winText;
+    public GameObject target;
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = ("Score: "+ score);
+
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            score += 1;
+            DestroyImmediate(target, true);
+        }
+        if (score == 10)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
